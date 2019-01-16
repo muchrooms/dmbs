0b2385ad2dffd5551dbc9c2449c348735df636fd
letli <letli@74ls74.org>
2019-01-16 22:27:07 +0800

    update timer

--- Timer/timer.c
+++ Timer/timer.c
@@ -3,13 +3,21 @@ int main ( void )
 {
     DDRC |= (1 << 7) ; // Set LED as output
     TCCR1B |= ( (1 << CS10) |  (1 << CS11) ); // Set up timer
+
+    int cnt = 0;
     for (;;)
     {
         // Check timer value in if statement , true when count matches 1/20 of a second
-        if ( TCNT1 >= 65525)
+        if ( TCNT1 >= 50000)
         {
-            PORTC ^= (1 << 7) ; // Toggle the LED
             TCNT1 = 0; // Reset timer value
+            cnt += 1;
         }
+        
+        if (cnt == 5) {
+            PORTC ^= (1 << 7) ; // Toggle the LED
+            cnt = 0;
+        }
+        
     }
 }
